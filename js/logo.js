fd6eb84bfa2403044aca1d9a86bbc31793f7805f
Ahmed Hasan <mr.batis@gmail.com>
2025-07-17 08:54:47 +0400

    Create logo.js

---
+++ js/logo.js
@@ -0,0 +1,19 @@
+// load-logo.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const logoContainer = document.querySelector(".logo");
+
+  fetch("src/aldarbi-logo.svg")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("SVG file not found or couldn't be loaded.");
+      }
+      return response.text();
+    })
+    .then(svgText => {
+      logoContainer.innerHTML = svgText;
+    })
+    .catch(error => {
+      console.error("Error loading SVG:", error);
+    });
+});
